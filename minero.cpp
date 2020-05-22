6db8c85557f2fbd79d49756acdcb0871a4bf1f57
Carolina Chamorro <cchamorrosaldana@deloitte.co.uk>
2020-05-22 22:28:10 +0100

    feat: logic readGame and pintaMina

--- minero.cpp
+++ minero.cpp
@@ -148,12 +148,58 @@ bool readGame(tJuego& juego, short nivel) {
 }
 
 void pintaMina(tMina& mina) {
+    system("cls");
+    
     for(int i = 0; i < mina.nfilas; i++ ) {
         for(int j = 0; j < mina.ncolumnas; j++) {
             cout << pintaCaracteres(mina.plano[i][j]);
         }
         cout << endl;
     }
+}
+
+int showMenu() {
+    char chooseOption = 0x00;
+    
+    while (chooseOption < '0' || chooseOption > '2') {
+        cout << "1. Jugar partida escala 1:1" << endl;
+        cout << "2. Jugar partida escala 1:3" << endl;
+        cout << "0. Salir" << endl;
+        
+        cout << "Elija opción: ";
+        cin >> chooseOption;
+    }
+    
+    return chooseOption-0x30;
+}
+
+int showTeclado() {
+    char chooseOption = 0x00;
+    
+    while (chooseOption < '0' || chooseOption > '2') {
+        cout << "1. Introducir movimientos por teclado" << endl;
+        cout << "2. Introducir movimientos por fichero" << endl;
+        cout << "0. Salir" << endl;
+        
+        cout << "Elija opción: ";
+        cin >> chooseOption;
+    }
+    
+    return chooseOption-0x30;
+}
+
+int showSiguienteNivel() {
+    char chooseOption = '0';
+    
+    while (chooseOption < '0' || chooseOption > '1') {
+        cout << "1. Jugar siguiente nivel" << endl;
+        cout << "0. Salir" << endl;
+        
+        cout << "Elija opción: ";
+        cin >> chooseOption;
+    }
+    
+    return chooseOption-0x30;
 }
 
 // void colorFondo(int color) {
@@ -186,8 +232,30 @@ void pintaMina(tMina& mina) {
 
 
 int main(int argc, const char * argv[]) {
-    tJuego juego;
+    tJuego juego ;
     short nivel = 1;
+    short option = 0;
+    bool escala = false; 
+
+    option = showMenu();
+
+    switch(option) {
+        case 1:
+            escala = false; 
+            break;
+
+        case 2:
+            escala = true; 
+            break;
+
+        case 3:
+            cout << "Hasta luego" << endl;
+            return 0;
+            break;
+
+        default:
+            break;
+    }
 
     readGame(juego, nivel);
     pintaMina(juego.mina);
