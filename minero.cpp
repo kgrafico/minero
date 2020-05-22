567092a8f4ffc5d7be0a999a0f451fda7da9041d
Carolina Chamorro <cchamorrosaldana@deloitte.co.uk>
2020-05-22 22:06:11 +0100

    feat: logic readGame and pintaMina

--- minero.cpp
+++ minero.cpp
@@ -8,6 +8,14 @@
 
 using namespace std;
 
+const int MAX = 50;
+
+typedef enum {
+    LIBRE, TIERRA, GEMA, PIEDRA, MURO, SALIDA, MINERO, DINAMITA
+} tCasilla;
+
+typedef tCasilla tPlano[MAX][MAX];
+
 typedef struct {
     tPlano plano;
     short nfilas;
@@ -24,16 +32,9 @@ typedef struct {
 } tJuego;
 
 typedef enum {
-    LIBRE, TIERRA, GEMA, PIEDRA, MURO, SALIDA, MINERO, DINAMITA
-} tCasilla;
-
-typedef tPlano = tCasilla[MAX][MAX];
-
-typedef enum {
      ARRIBA, ABAJO, DCHA, IZDA, SALIR, NADA, TNT
 } tTecla;
 
-const int MAX = 50;
 const string archivosNiveles[] = {"1.txt", "2.txt", "3.txt", "4.txt"};
 char planoCaracteres[3*MAX][3*MAX];
 int planoColores[3*MAX][3*MAX];
@@ -50,26 +51,27 @@ tCasilla rellenaCasilla(char c) {
             break;
 
         case 'G':
-            return GEMA
+            return GEMA;
             break;
         
         case 'P':
-            return PIEDRA
+            return PIEDRA;
             break;
         
         case 'M':
-            return MURO
+            return MURO;
             break;
         
         case 'S':
-            return SALIDA
+            return SALIDA;
             break;
         
         case 'J':
-            return MINERO
+            return MINERO;
             break;
         
         default:
+        exit(-1);
         break;
     }
 }
@@ -106,24 +108,36 @@ char pintaCaracteres(tCasilla c) {
             break;
         
         default:
+        exit(-1);
         break;
     }
 }
 
-bool readGame(tJuego juego, short nivel) {
+bool readGame(tJuego& juego, short nivel) {
     ifstream archivo;
     string fila;
 
     archivo.open(archivosNiveles[nivel-1], ios::in);
     if (!archivo.is_open()) {
-        cout << "¡No se ha podido abrir el archivo!" << endl;
+        cout << "¡No se ha podido abrir el archivo "<< archivosNiveles[nivel-1] <<"!" << endl;
         return false;
     }
 
     archivo >> juego.mina.nfilas >> juego.mina.ncolumnas;
+    getline(archivo, fila);
+
+    if (juego.mina.nfilas > MAX) {
+        cout << "Filas leidas (" << juego.mina.nfilas << ") mayor que maximo (" << MAX << ")" << endl;
+        return false;
+    }
+
+    if (juego.mina.ncolumnas > MAX) {
+        cout << "Columnas leidas (" << juego.mina.ncolumnas << ") mayor que maximo (" << MAX << ")" << endl;
+        return false;
+    }
 
     for (int i = 0; i < juego.mina.nfilas; i++) {
-        archivo.getline(fila, juego.mina.ncolumnas);
+        getline(archivo, fila);
         for (int j = 0; j < juego.mina.ncolumnas; j++) {
             juego.mina.plano[i][j] = rellenaCasilla(fila[j]);
         }
@@ -133,19 +147,19 @@ bool readGame(tJuego juego, short nivel) {
     return true;
 }
 
-void pintaMina(tMina mina) {
+void pintaMina(tMina& mina) {
     for(int i = 0; i < mina.nfilas; i++ ) {
-        for(int j = 0; i < mina.ncolumnas; j++) {
-            cout << mina.plano[i][j];
+        for(int j = 0; j < mina.ncolumnas; j++) {
+            cout << pintaCaracteres(mina.plano[i][j]);
         }
         cout << endl;
     }
 }
 
-void colorFondo(int color) {
- HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
- SetConsoleTextAttribute(handle, 15 | (color << 4));
-}
+// void colorFondo(int color) {
+//  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+//  SetConsoleTextAttribute(handle, 15 | (color << 4));
+// }
 
 
 // void writeGame(tPlay play) {
@@ -173,10 +187,10 @@ void colorFondo(int color) {
 
 int main(int argc, const char * argv[]) {
     tJuego juego;
-    short nivel;
+    short nivel = 1;
 
     readGame(juego, nivel);
-    pintaMina(mina);
+    pintaMina(juego.mina);
 
     return 0;
 }
